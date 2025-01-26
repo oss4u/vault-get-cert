deb9fa2557d9668a8e0b1cb70e89fbb82559d263
Marc Ende <marc.ende_ext@external.mail.schwarz>
2025-01-26 19:16:01 +0100

    chore: added helper functions

--- internal/networking/ip-address.go
+++ internal/networking/ip-address.go
@@ -5,17 +5,23 @@ import (
 	"net"
 )
 
+// GetIpAddresses returns a list of non-loopback IPv4 addresses.
 func GetIpAddresses() ([]string, error) {
-	var result []string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return []string{}, fmt.Errorf("failed to get interface addresses: %w", err)
+		return nil, fmt.Errorf("failed to get interface addresses: %w", err)
 	}
+
+	var result []string
 	for _, addr := range addrs {
-		ipNet, ok := addr.(*net.IPNet)
-		if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+		if ipNet, ok := addr.(*net.IPNet); ok && isNonLoopbackIPv4(ipNet) {
 			result = append(result, ipNet.IP.String())
 		}
 	}
 	return result, nil
+}
+
+// isNonLoopbackIPv4 checks if the given IPNet is a non-loopback IPv4 address.
+func isNonLoopbackIPv4(ipNet *net.IPNet) bool {
+	return !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil
 }
